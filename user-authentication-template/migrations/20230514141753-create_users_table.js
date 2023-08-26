fdc7341c176198dd559c3e635d1f50a8969106ac
Thiago Carvalho da Silva <maddogxx@gmail.com>
2023-08-26 14:19:38 -0300

    Ajuste no script de migrations para sqlite3

--- user-authentication-template/migrations/20230514141753-create_users_table.js
+++ user-authentication-template/migrations/20230514141753-create_users_table.js
@@ -29,11 +29,13 @@ module.exports = {
 
       createdAt: {
         type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
         allowNull: false
       },
 
       updatedAt: {
         type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),        
         allowNull: false
       }
     })
